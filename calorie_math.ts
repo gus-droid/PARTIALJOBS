d5266ee48e1978755c300c7a7a0cb8c9f8152b84
Desperationis <fork_important@pm.me>
2024-09-14 15:43:37 -0400

    Update calorie_math

--- calorie_math.ts
+++ calorie_math.ts
@@ -1,1 +1,13 @@
-export function calculate_bmr(weight: int
+
+// Calculate BMR (Calories burned doing nothing)
+export function calculate_bmr(weight: number, height: number, age: number, male: bool): number {
+    if (male)
+        return 66.47 + (6.24 * weight) + (12.7 * height) - (6.755 * age);
+
+    return 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+}
+
+// Calculate difference in calories from normal to lose x pounds per week.
+export function calorie_delta_goal(lbs_per_week: number): number {
+    return (3500.0 / 7.0) * lbs_per_week;
+}
