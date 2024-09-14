fc9519bcd745e94ca6996767e5d4d62c663d3420
Desperationis <fork_important@pm.me>
2024-09-14 16:26:03 -0400

    Update calorie_math

--- calorie_math.ts
+++ calorie_math.ts
@@ -1,3 +1,11 @@
+enum ExerciseLevel {
+    Sedentary,
+    Light,
+    Moderate,
+    Extreme
+};
+
+
 
 // Calculate BMR (Calories burned doing nothing)
 export function calculate_bmr(weight: number, height: number, age: number, male: bool): number {
@@ -11,3 +19,20 @@ export function calculate_bmr(weight: number, height: number, age: number, male:
 export function calorie_delta_goal(lbs_per_week: number): number {
     return (3500.0 / 7.0) * lbs_per_week;
 }
+
+export function energy_delta(bmr: number, level: ExerciseLevel): number {
+    switch(level) {
+        case ExerciseLevel.Sedentary:
+            return bmr * 0.2;
+        case ExerciseLevel.Light:
+            return bmr * 0.375;
+        case ExerciseLevel.Moderate:
+            return bmr * 0.5;
+        case ExerciseLevel.Extreme:
+            return bmr * 0.9;
+    }
+}
+
+
+
+
