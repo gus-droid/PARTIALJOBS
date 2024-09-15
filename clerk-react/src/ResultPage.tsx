80f99c7dc457b4f48fdb45bbc63b5674bad33a53
Desperationis <fork_important@pm.me>
2024-09-14 20:13:22 -0400

    Do calorie stuff

--- clerk-react/src/ResultPage.tsx
+++ clerk-react/src/ResultPage.tsx
@@ -1,10 +1,22 @@
 import { useLocation } from 'react-router-dom';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { ExerciseLevel, calculate_bmr, calorie_delta_goal, energy_delta } from './calorie_math';
 
 
 const ResultPage = () => {
     const location = useLocation();
     const formData = location.state as { [key: string]: string };
+
+    const age = parseFloat(formData["age"]);
+    const height = parseFloat(formData["height"]);
+    const weight = parseFloat(formData["currentWeight"]);
+    const target_weight = parseFloat(formData["goalWeight"]);
+    const budget = parseFloat(formData["budget"]);
+    const is_male = formData["sex"] == "male";
+
+    const bmr: number = calculate_bmr(weight, height, age, is_male);
+    const energy_cal: number = energy_delta(bmr, ExerciseLevel.Moderate);
+    const in_12_weeks: number = calorie_delta_goal(-(weight - target_weight)/12);
 
     return (
         <div>
@@ -17,6 +29,8 @@ const ResultPage = () => {
             <SignedIn>
                 <UserButton />
             </SignedIn>
+            <p>You burn {bmr} calories from doing nothing. You also burn {energy_cal} from moderate energy activites. This means you can eat {bmr + energy_cal} calories per day without any change in weight.</p>
+            <p>You will need a calorie deficit of {in_12_weeks} calories per day to lose {weight - target_weight} pounds in 12 weeks. This means that, if you want to lose weight, you should eat around {bmr + energy_cal + in_12_weeks} calories per day for 12 weeks to lose {weight - target_weight}</p>
         </div>
     );
 };
---
+++ clerk-react/src/calorie_math.ts
@@ -0,0 +1,38 @@
+export enum ExerciseLevel {
+    Sedentary,
+    Light,
+    Moderate,
+    Extreme
+};
+
+
+
+// Calculate BMR (Calories burned doing nothing)
+export function calculate_bmr(weight: number, height: number, age: number, male: bool): number {
+    if (male)
+        return 66.47 + (6.24 * weight) + (12.7 * height) - (6.755 * age);
+
+    return 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+}
+
+// Calculate difference in calories from normal to lose x pounds per week.
+export function calorie_delta_goal(lbs_per_week: number): number {
+    return (3500.0 / 7.0) * lbs_per_week;
+}
+
+export function energy_delta(bmr: number, level: ExerciseLevel): number {
+    switch(level) {
+        case ExerciseLevel.Sedentary:
+            return bmr * 0.2;
+        case ExerciseLevel.Light:
+            return bmr * 0.375;
+        case ExerciseLevel.Moderate:
+            return bmr * 0.5;
+        case ExerciseLevel.Extreme:
+            return bmr * 0.9;
+    }
+}
+
+
+
+export default { calculate_bmr };
