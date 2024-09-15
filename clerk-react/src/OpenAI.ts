dbf188209908c20c5fdff0f1fc419582a938a8df
Desperationis <fork_important@pm.me>
2024-09-15 05:05:47 -0400

    Optimize

--- clerk-react/src/OpenAI.ts
+++ clerk-react/src/OpenAI.ts
@@ -68,7 +68,9 @@ export async function get_meal_plan(carbs: number, protein: number, fat: number)
 
 
   // Correct data with NutritionNinja
-  for(const index in data["breakfast"]["ingredients"]) {
+  const times = ["breakfast", "lunch", "dinner"];
+  times.forEach((time: string) => {
+  for(const index in data[time]["ingredients"]) {
     var ingredient = data.breakfast.ingredients[index];
       var ingredient_name = ingredient.name;
       var ingredient_amount = ingredient.quantity;
@@ -78,30 +80,7 @@ export async function get_meal_plan(carbs: number, protein: number, fat: number)
       ingredient.protein = corrected_data.items[0].protein_g;
       ingredient.fat = corrected_data.items[0].fat_total_g;
       ingredient.carbs = corrected_data.items[0].carbohydrates_total_g;
-    }
-  for(const index in data["lunch"]["ingredients"]) {
-    var ingredient = data.lunch.ingredients[index];
-      var ingredient_name = ingredient.name;
-      var ingredient_amount = ingredient.quantity;
-      var prompt = ingredient_amount + " of " + ingredient_name;
-
-      var corrected_data = await fetchNutritionData(prompt);
-      ingredient.protein = corrected_data.items[0].protein_g;
-      ingredient.fat = corrected_data.items[0].fat_total_g;
-      ingredient.carbs = corrected_data.items[0].carbohydrates_total_g;
-    }
-  for(const index in data["dinner"]["ingredients"]) {
-    var ingredient = data.dinner.ingredients[index];
-      var ingredient_name = ingredient.name;
-      var ingredient_amount = ingredient.quantity;
-      var prompt = ingredient_amount + " of " + ingredient_name;
-
-      var corrected_data = await fetchNutritionData(prompt);
-      ingredient.protein = corrected_data.items[0].protein_g;
-      ingredient.fat = corrected_data.items[0].fat_total_g;
-      ingredient.carbs = corrected_data.items[0].carbohydrates_total_g;
-    }
-  
+    }};
 
   return data;
 }
